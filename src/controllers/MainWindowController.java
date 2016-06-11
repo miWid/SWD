843c23bffefa6b30193b208929aff4f0fceaa6e0
Michal <michailwid@gmail.com>
2016-06-11 19:14:48 +0200

    Poprawki + reszta okien

--- src/controllers/MainWindowController.java
+++ src/controllers/MainWindowController.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 
