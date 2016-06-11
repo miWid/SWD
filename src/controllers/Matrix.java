ddb6ff3db58f6a6ce9928b761f3a54cc51238330
qzmp <lukasz.golebiowski@mail.ru>
2016-06-11 23:32:36 +0200

    Matrix fixes

--- src/controllers/Matrix.java
+++ src/controllers/Matrix.java
@@ -9,6 +9,7 @@ public class Matrix {
     private final static double[] RI = {0, 0, 0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.45, 1.49};
 
     private double[][] data;
+    private double[] columnSumsBeforeNormalization;
 
     private double countRowSum(int rowIndex) {
         double rowSum = 0;
@@ -28,7 +29,7 @@ public class Matrix {
 
     private int countRowNumber(int valueCount) {
         int rowNumber = 0;
-        int acc = 0;
+        int acc = 1;
         while(valueCount > 0) {
             rowNumber++;
             valueCount -= acc++;
@@ -43,8 +44,8 @@ public class Matrix {
     public void setValues(double[] values) {
         int matrixSize = countRowNumber(values.length);
         data = new double[matrixSize][matrixSize];
-        int processedValues = 0;
 
+        int processedValues = 0;
         for(int rowIndex = 0; rowIndex < data.length - 1; rowIndex++) {
             for(int columnIndex = rowIndex + 1; columnIndex < data.length; columnIndex++) {
                 data[rowIndex][columnIndex] = values[processedValues++];
@@ -52,9 +53,9 @@ public class Matrix {
         }
 
         processedValues = 0;
-        for(int columnIndex = 0; columnIndex < data.length; columnIndex++) {
-            for(int rowIndex = 1; rowIndex < data.length; rowIndex++) {
-                data[rowIndex][columnIndex] = 1 / values[processedValues];
+        for(int columnIndex = 0; columnIndex < data.length - 1; columnIndex++) {
+            for(int rowIndex = columnIndex + 1; rowIndex < data.length; rowIndex++) {
+                data[rowIndex][columnIndex] = 1 / values[processedValues++];
             }
         }
 
@@ -64,9 +65,11 @@ public class Matrix {
     }
 
     public void normalize() {
+        columnSumsBeforeNormalization = new double[data.length];
         double columnSum;
         for(int columnIndex = 0; columnIndex < data.length; columnIndex++) {
             columnSum = countColumnSum(columnIndex);
+            columnSumsBeforeNormalization[columnIndex] = columnSum;
 
             for(int rowIndex = 0; rowIndex < data.length; rowIndex++) {
                 data[rowIndex][columnIndex] /= columnSum;
@@ -75,7 +78,7 @@ public class Matrix {
     }
 
     public double[] getPreferencesVector() {
-        double[] preferencesVector = new double[3];
+        double[] preferencesVector = new double[data.length];
         double rowSum;
         for(int rowIndex = 0; rowIndex < data.length; rowIndex++) {
             rowSum = countRowSum(rowIndex);
@@ -91,10 +94,8 @@ public class Matrix {
 
     public boolean isConsistent(double acceptedRatio) {
         double CI = 0;
-        for(int rowIndex = 0; rowIndex < data.length; rowIndex++) {
-            for(int columnIndex = 0; columnIndex < data[rowIndex].length; columnIndex++) {
-                CI += countRowSum(rowIndex) * countColumnSum(columnIndex);
-            }
+        for(int i = 0; i < data.length; i++) {
+            CI += countRowSum(i)/data.length * columnSumsBeforeNormalization[i];
         }
         CI -= data.length;
         CI /= data.length - 1;
