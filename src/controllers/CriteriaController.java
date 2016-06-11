8bdc2bddcd3af054171038efb1ab12392a23985b
Michal <michailwid@gmail.com>
2016-06-11 23:02:37 +0200

    controllery v2.0

--- src/controllers/CriteriaController.java
+++ src/controllers/CriteriaController.java
@@ -3,11 +3,13 @@ package controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,13 +27,19 @@ public class CriteriaController implements Initializable {
     @FXML
     private Button cofnij0, dalej0;
     @FXML
+    private Label kryteria_spojnosc;
+    @FXML
     private Slider krytS1,krytS2,krytS3,krytS4,krytS5,krytS6,krytS7,krytS8,krytS9,krytS10;
+    @FXML
+    private SplitPane krytPane;
 
     private Scene prevScene;
+    public static Scene currScene;
     private ArrayList<String> selectedProviders = new ArrayList<>();
-    private ArrayList<Integer> selectedPreferences = new ArrayList<>();
+    private static double[] values = new double[10];
 
     void initData(List<String> providers) {
+        setCurrentScene(krytPane.getScene());
         this.selectedProviders.addAll(providers);
     }
 
@@ -48,15 +56,19 @@ public class CriteriaController implements Initializable {
     @FXML
     private void nextPage(ActionEvent event) throws IOException {
 
-        if(true){
+        if(checkIfConsistent()){
 
             Stage stage = (Stage) dalej0.getScene().getWindow();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/warianty.fxml"));
             Scene scene = new Scene(loader.load());
-            stage.setScene(scene);
             ChoicesController controller = loader.<ChoicesController>getController();
-            controller.initData(selectedProviders);
-            controller.setPreviousScene(dalej0.getScene());
+            if(controller.currScene != null)
+                stage.setScene(controller.currScene);
+            else {
+                controller.initData(selectedProviders);
+                controller.setPreviousScene(dalej0.getScene());
+                stage.setScene(scene);
+            }
             stage.show();
 
         }
@@ -73,13 +85,49 @@ public class CriteriaController implements Initializable {
 
     }
 
+    private boolean checkIfConsistent() {
+        return kryteria_spojnosc.getText().equals("TAK");
+    }
+
 
     public void setPreviousScene(Scene prevScene){
         this.prevScene = prevScene;
     }
 
+    public void setCurrentScene(Scene currScene){
+        this.currScene = currScene;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    @FXML
+    private void isConsistent(Event event) {
+
+
+        values[0] = krytS1.getValue();
+        values[1] = krytS2.getValue();
+        values[2] = krytS3.getValue();
+        values[3] = krytS4.getValue();
+        values[4] = krytS5.getValue();
+        values[5] = krytS6.getValue();
+        values[6] = krytS7.getValue();
+        values[7] = krytS8.getValue();
+        values[8] = krytS9.getValue();
+        values[9] = krytS10.getValue();
+
+        Matrix matrix = new Matrix(values);
+        matrix.normalize();
+
+        if(matrix.isConsistent()){
+            kryteria_spojnosc.setText("TAK");
+            kryteria_spojnosc.setTextFill(Paint.valueOf("#029940"));
+        }
+        else {
+            kryteria_spojnosc.setText("NIE");
+            kryteria_spojnosc.setTextFill(Paint.valueOf("#ea0404"));
+        }
 
     }
 }
